d4d2e9c52588a476cb40559a7b46cee5b71ceb95
Clemens Weissbacher <4752766+weimens@users.noreply.github.com>
2020-02-27 19:35:03 +0100

    code formatting

--- heatmapprovider.h
+++ heatmapprovider.h
@@ -31,20 +31,20 @@ public:
   // better.
   QImage requestImage(const QString &id, QSize *size,
                       const QSize &requestedSize) override {
-      QImage image;
-      if(id.startsWith("legend")){
-          image = legend(requestedSize);
-      }else{
-        qreal ratio = 1.0;
-        if (requestedSize.height() != 0) {
-          ratio =
-              requestedSize.width() / static_cast<qreal>(requestedSize.height());
-        }
-
-        image = updateHeatMapPlot(ratio, requestedSize);
+    QImage image;
+    if (id.startsWith("legend")) {
+      image = legend(requestedSize);
+    } else {
+      qreal ratio = 1.0;
+      if (requestedSize.height() != 0) {
+        ratio =
+            requestedSize.width() / static_cast<qreal>(requestedSize.height());
       }
-        size->setWidth(image.width());
-        size->setHeight(image.height());
+
+      image = updateHeatMapPlot(ratio, requestedSize);
+    }
+    size->setWidth(image.width());
+    size->setHeight(image.height());
 
     return image;
   }
@@ -87,21 +87,21 @@ private:
       qRgba(0, 104, 55, 255),
   };
 
-  QImage legend(QSize size){
+  QImage legend(QSize size) {
 
-      QImage image = QImage(size.width(), 1, QImage::Format_Indexed8);
-      int colorCount = m_colors.size() - 1;
-      image.setColorCount(colorCount + 1);
-      for (int i = 0; i < m_colors.size(); ++i) {
-          image.setColor(i, m_colors[i]);
-      }
+    QImage image = QImage(size.width(), 1, QImage::Format_Indexed8);
+    int colorCount = m_colors.size() - 1;
+    image.setColorCount(colorCount + 1);
+    for (int i = 0; i < m_colors.size(); ++i) {
+      image.setColor(i, m_colors[i]);
+    }
 
-      for (int i = 0; i < size.width(); ++i) {
-          int color = floor(i/static_cast<double>(size.width())*colorCount);
-          image.setPixel(i, 0, color);
-      }
+    for (int i = 0; i < size.width(); ++i) {
+      int color = floor(i / static_cast<double>(size.width()) * colorCount);
+      image.setPixel(i, 0, color);
+    }
 
-      return image;
+    return image;
   }
 
   QImage updateHeatMapPlot(qreal ratio, QSize size) {

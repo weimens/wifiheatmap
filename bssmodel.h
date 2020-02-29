014a9f2cdb2eaed6587dc8ae53ba74295772225a
Clemens Weissbacher <4752766+weimens@users.noreply.github.com>
2020-02-29 19:10:48 +0100

    Move implementation to bssmodel.cpp file.

--- bssmodel.h
+++ bssmodel.h
@@ -7,58 +7,18 @@ class BssModel : public QStandardItemModel {
   Q_OBJECT
 
 public:
-  BssModel(QObject *parent = nullptr) : QStandardItemModel(parent) {
-    setItemRoleNames(
-        {{Qt::DisplayRole, "display"}, {Qt::CheckStateRole, "checkstate"}});
-    QObject::connect(this, &BssModel::dataChanged, this,
-                     &BssModel::selectionChanged);
-    setHorizontalHeaderLabels(QStringList() << "ssid"
-                                            << "bss"
-                                            << "freq"
-                                            << "ch"
-                                            << "");
-  }
+  BssModel(QObject *parent = nullptr);
 
 public slots:
   void selectionChanged(const QModelIndex &topLeft,
                         const QModelIndex &bottomRight,
-                        const QVector<int> &roles) {
-    if (std::find(roles.begin(), roles.end(), Qt::CheckStateRole) !=
-        roles.end()) {
-      QList<QString> bss = getSelectedBss();
-      emit selectedBssChanged(bss);
-    }
-  }
+                        const QVector<int> &roles);
 
-  void addBss(QString bssid, QString ssid, qreal freq, qreal channel) {
-    QStandardItem *item1 = new QStandardItem(ssid);
-    QStandardItem *item2 = new QStandardItem(bssid);
-    QStandardItem *item3 = new QStandardItem();
-    item3->setData(freq, Qt::DisplayRole);
-    QStandardItem *item4 = new QStandardItem();
-    item4->setData(channel, Qt::DisplayRole);
-    QStandardItem *item5 = new QStandardItem();
-    item5->setCheckable(true);
-    item5->setData(Qt::Unchecked, Qt::CheckStateRole);
-    appendRow(QList<QStandardItem *>()
-              << item1 << item2 << item3 << item4 << item5);
-  }
+  void addBss(QString bssid, QString ssid, qreal freq, qreal channel);
 
 signals:
   void selectedBssChanged(QList<QString>);
 
 private:
-  QList<QString> getSelectedBss() {
-    QList<QString> ret;
-    for (int i = 0; i < rowCount(); ++i) {
-      QStandardItem *itemCheck =
-          static_cast<QStandardItem *>(itemFromIndex(index(i, 4)));
-      if (itemCheck->data(Qt::CheckStateRole) == Qt::Checked) {
-        QStandardItem *item =
-            static_cast<QStandardItem *>(itemFromIndex(index(i, 1)));
-        ret.append(item->data(Qt::DisplayRole).toString());
-      }
-    }
-    return ret;
-  }
+  QList<QString> getSelectedBss();
 };
